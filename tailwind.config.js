dc2f444947835529df12ed1769e18827ff5c1763
koloseki <koloseki2002@gmail.com>
2023-08-16 18:27:20 +0200

    Creating HTML structure for the project and created design for phones and tablets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-vanilla': '#DCC1AB',
+        'pomona-green ': '#1E3D2F',
+        'chinese-black': '#111111',
+        'isabelline': '#ECF1F5',
+        'anti-flash-white': '#F5F0EC',
+
+    },
   },
   plugins: [],
 }
-
+}
