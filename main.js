5603b7199e515f3e815fab7fc3ba5656cca53f08
koloseki <koloseki2002@gmail.com>
2023-08-20 20:58:27 +0200

    Initialized MasonryJs ,finished projects section and added a few mini animations

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+import Masonry from 'masonry-layout'
+
+window.onload = function() {
+    // Tworzenie instancji Masonry
+    const grid = document.querySelector('.grid');
+    const msnry = new Masonry(grid);
+};
 
 
 
 
-console.log('hello world')
+let photoGallery = document.querySelector('.grid');
 
 
+const offerButton = document.querySelector('#offerButton');
+let dialog = document.querySelector('dialog');
+const offerArrow = document.querySelector('.offerArrow');
+offerButton.addEventListener('click', function () {
+    dialog.toggleAttribute('open');
+    offerArrow.classList.toggle('rotate');
+});
+
+
+const searchBarToggle = document.querySelector('.searchBarToggle');
+let searchBar = document.querySelector('.searchBar');
+searchBarToggle.addEventListener('click', function () {
+    searchBar.classList.toggle('hidden');
+});
